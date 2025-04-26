8fa8233ebbbe3f77d74de4ce1ef38e3e4a5a417b
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2025-04-26 23:48:56 +0200

    add scroll to top fab instead

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,13 +1,37 @@
 import { FaSolidArrowUp, FaSolidChevronUp } from "solid-icons/fa";
+import { createEffect, createSignal, type Accessor } from "solid-js";
 
-const ScrollToTop = () => {
+export interface ClickEvt {
+  clicked: boolean;
+}
+
+const ScrollToTop = (props: {
+  size?: string;
+  trigger?: Accessor<ClickEvt>;
+}) => {
+  const [size] = createSignal(props.size || "1.2rem");
+
+  createEffect(() => {
+    if (props?.trigger?.().clicked) {
+      scrollToTop();
+    }
+  });
+  
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const handleScrollToTop = (e: MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (props.trigger) return;
+    scrollToTop();
+  };
+
   return (
     <FaSolidArrowUp
-      onClick={scrollToTop}
+      size={size()}
+      onClick={handleScrollToTop}
       class="hover:cursor-pointer"
     />
   );
---
+++ src/components/ScrollToTopFab.tsx
@@ -0,0 +1,18 @@
+import { createSignal } from "solid-js";
+import ScrollToTop, { type ClickEvt } from "./ScrollToTop";
+
+const ScrollToTopFab = () => {
+  const [clicked, setClicked] = createSignal<ClickEvt>({ clicked: false });
+
+  return (
+    <div
+      onClick={() => setClicked({ clicked: true })}
+      id="scroll-to-top-fab"
+      class="hover:cursor-pointer fixed bottom-10 right-10 z-10000 background-white shadow-xl rounded-full p-3 bg-white border-gray-300 border"
+    >
+      <ScrollToTop size="1.2rem" trigger={clicked} />
+    </div>
+  );
+};
+
+export default ScrollToTopFab;
