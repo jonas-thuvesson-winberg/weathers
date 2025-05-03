085cbf619a5ee364221a7568ec792d93a21d1cf9
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2025-05-03 22:21:10 +0200

    Make fab position dynamic

--- src/components/ScrollToTopFab.tsx
+++ src/components/ScrollToTopFab.tsx
@@ -8,7 +8,7 @@ const ScrollToTopFab = () => {
     <div
       onClick={() => setClicked({ clicked: true })}
       id="scroll-to-top-fab"
-      class="hover:cursor-pointer fixed bottom-5 right-5 z-10000 background-white shadow-xl rounded-full p-6 bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-600 border-2"
+      class="hover:cursor-pointer fixed bottom-5 right-5 lg:right-25 xl:right-40 z-10000 background-white shadow-xl rounded-full p-6 bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-600 border-2"
     >
       <ScrollToTop size="1.2rem" trigger={clicked} />
     </div>
